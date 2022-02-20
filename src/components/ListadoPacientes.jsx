6cf05b0c490efbc102e266b6704552a42bb568cc
jorneylm <jorney95@live.com>
2022-02-19 22:54:07 -0600

    add conditionales

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -5,17 +5,31 @@ const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-      <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
-      <p className="text-lg mt-5 mb-5 text-center">
-        Administra tus {' '}
-        <spam className="text-indigo-600 font-bold">Pacientes y Citas</spam>
-      </p>
-      {pacientes.map(pacient => (
-        <Paciente
-          key={pacient.id}
-          pacient={pacient}
-        />
-      ))}
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+          <p className="text-lg mt-5 mb-5 text-center">
+            Administra tus {' '}
+            <spam className="text-indigo-600 font-bold">Pacientes y Citas</spam>
+          </p>
+          {pacientes.map(pacient => (
+            <Paciente
+              key={pacient.id}
+              pacient={pacient}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+          <p className="text-lg mt-5 mb-5 text-center">
+            Comienza agregando pacientes {' '}
+            <spam className="text-indigo-600 font-bold">ya mismo</spam>
+          </p>
+        </>
+      )}
+
+
 
     </div>
   )
